fe601d01ab81323d7ec0f1b2e3c23b5b0030ac93
otonashixav <xavier.loh.yz@gmail.com>
2018-11-03 21:05:49 +0800

    added some skeleton code

--- mBot-default-program/A-maze-ing_mBot.ino
+++ mBot-default-program/A-maze-ing_mBot.ino
@@ -1,4 +1,4 @@
-#include <Arduino.h> // should be included automatically when compiled
+#include <Arduino.h> // should be included automatically when compiled; includes stdbool
 #include "pitches.h" // note definitions for playing MUSIC
 
 // time definitions
@@ -16,14 +16,40 @@
 #define MOTOR_L        // left motor
 #define MOTOR_R        // right motor
 
+// motor functions
+void turn_left() {
+
+}
+
+void turn_right() {
+
+}
+
+void turn_180() {
+
+}
+
+void move_forward() {
+
+}
+
+// victory theme
+
+
+void play_theme() {
+
+}
+
+// sensor functions
+
 /**
  * Sends an ultrasonic pulse and waits for the echo. The timeout is defined
  * by ULTRASONIC_TIMEOUT. Returns the time in microseconds before the echo is
- * heard. 
+ * heard. Consider replacing with boolean function for "close enough to wall"
  * 
  * @return  Time taken for the ultrasonic pulse to return in microseconds.
  */
-long read_ultrasonic() {
+long read_ultrasonic_sensor() {
     pinMode(ULTRASONIC, OUTPUT);
     digitalWrite(ULTRASONIC, HIGH);
     delayMicroseconds(2);
@@ -32,10 +58,33 @@ long read_ultrasonic() {
     return pulseIn(ULTRASONIC, HIGH, ULTRASONIC_TIMEOUT);
 }
 
+bool read_line_sensor() {
+    
+}
+
+// challenge functions
+
+int solve_color() {
+
+}
+
+int solve_sound() {
+
+}
+
+int solve_challenge() {
+
+}
+
 void setup() {
 
 }
 
 void loop() {
+    if (read_line_sensor() == HIGH) {
+        solve_challenge();
+    } else {
+        // read ir sensors and determine if correction necessary, else straight
 
+    }
 }
